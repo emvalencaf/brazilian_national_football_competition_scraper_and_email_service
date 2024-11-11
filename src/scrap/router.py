b797c72c95ae231caec751ee0ed5292421e197ed
emvalencaf <edsonmvf@gmail.com>
2024-11-11 17:36:50 -0300

    :bug: fix: Fix Error
    send_email_with_attachment was being invoke as a async function at the router instead of a sync function.

--- src/scrap/router.py
+++ src/scrap/router.py
@@ -17,16 +17,17 @@ async def send_email(request: RequestEmail):
 
         # Get the dataframe asynchronously
         df = await get_all_competition_dataframe(ids=ids)
-        
+
         # Create the Excel file in memory
         excel_file = BytesIO()
         df.to_excel(excel_file, index=False, engine='openpyxl')
         excel_file.seek(0)
 
         # Send the email with the attached Excel file
-        await send_email_with_attachment(request.email, excel_file)
+        send_email_with_attachment(request.email, excel_file)
 
         return {"message": "Excel file was successfully sent!"}
     
     except Exception as e:
+        print(e)
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
